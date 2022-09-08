894163543009427da4df088599b25f2edf4a87de
ShekinahSJ <100009660+ShekinahSJ@users.noreply.github.com>
2022-09-08 21:52:59 +0530

    Nineth Commit

--- Main.py
+++ Main.py
@@ -28,12 +28,11 @@ class Stack:
     """
     It prints all the elements of stack.
     """
-    # Write your code here
-    if (self.head !=None):
-      temp=self.head
-      while temp.next!=None:
-        temp=temp.next
-        print(temp,"=>")
+    if(self.head != None):
+      temp = self.head
+      while(temp.next != None):
+        print(temp.data, end = "=>")
+        temp = temp.next
       else:
         print(temp.data,"None", sep="=>")
     else:
